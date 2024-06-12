55d95010efff4a537a06449f68aa5b15ae810ae3
Art <honeydewart@gmail.com>
2024-06-12 11:25:26 +0800

    make render-build.sh executable

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
 bundle install
-chmod +x ./bin/rails
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:migrate
