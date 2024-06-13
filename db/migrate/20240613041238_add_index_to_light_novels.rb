c8c9bb7dc2d18edafe2d356ce99c8af14b342629
Art <honeydewart@gmail.com>
2024-06-13 12:17:38 +0800

    fixed date time validator

--- db/migrate/20240613041238_add_index_to_light_novels.rb
+++ db/migrate/20240613041238_add_index_to_light_novels.rb
@@ -1,6 +1,5 @@
 class AddIndexToLightNovels < ActiveRecord::Migration[7.1]
   def change
-    add_column :light_novels, :title, :string
     add_index :light_novels, :title, unique: true
   end
 end
